1403c6bc0a2241871b72495d740b1fe5c69daf2d
Syed Aman <sna274@nyu.edu>
2017-09-30 15:00:01 -0500

    create The API using the AWS SDK for retrieving notes from the DynamoDB notes database

---
+++ get.js
@@ -0,0 +1,32 @@
+/**
+ * The API using the AWS SDK for retrieving notes from the DynamoDB notes
+ * database.
+ */
+
+import requestDynamoDB from './lib/dynamodb'
+import buildResponse from './lib/responses'
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: 'notes',
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    }
+  }
+
+  try {
+    // retrieve a note from the notes table in DynamoDB
+    const result = requestDynamoDB('get', params)
+
+    if (result.Item) {
+      callback(null, buildResponse(200, result.Item)
+    } else {
+      callback(null, buildResponse(500, { status: false, error: "note not found!" }))
+    }
+  } catch (error) {
+    console.log(error)
+
+    callback(null, buildResponse(500, { status: false }))
+  }
+}
