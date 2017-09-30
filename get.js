90eaeb562e866a4ebbd411615073df78f66221fe
Syed Aman <sna274@nyu.edu>
2017-09-30 17:47:06 -0500

    Improve docs in the get api

--- get.js
+++ get.js
@@ -6,11 +6,46 @@
 import requestDynamoDB from './lib/dynamodb'
 import buildResponse from './lib/responses'
 
+/**
+ * A lambda function that retrieves notes from the DynamoDB database when
+ * a GET event is received.
+ * @method main
+ * @param  {Object} event - All the information about the event that
+ * triggered this Lambda function. The request to create a note.
+ * @param  {Object} context - The runtime the Lambda function is executing in.
+ * @param  {Function} callback - The function called after Lambda function is
+ * executed with the results or errors. AWS will respond to the HTTP request
+ * with it.
+ * @return {void}
+ */
 export async function main(event, context, callback) {
+  /**
+   * The options used to retrieve notes from the DynamoDB database, specifying
+   * table name, what item, the primary id, etc.
+   * @type {Object}
+   */
   const params = {
+    /**
+     * Name of the table of the DynamoDB to update.
+     * @type {String}
+     */
     TableName: 'notes',
+    /**
+     * Defines the partition key and sort key of the note to be retrieved in
+     * the table.
+     * @type {Object}
+     */
     Key: {
+      /**
+       * The unique identifier of the user that has the note.
+       * @type {String}
+       */
       userId: event.requestContext.identity.cognitoIdentityId,
+      /**
+       * The unique identifier for the note, provider by the parameter in the
+       * path.
+       * @type {String}
+       */
       noteId: event.pathParameters.id
     }
   }
